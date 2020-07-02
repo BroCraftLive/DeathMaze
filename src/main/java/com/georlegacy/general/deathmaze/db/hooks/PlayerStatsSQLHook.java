6f0aa87e0d958f5052f0bd918144fa15e6a72d5b
James Conway <james@xsc.co,uk>
2020-07-02 14:46:12 +0100

    temporary constructor fix to allow ci compilation

--- src/main/java/com/georlegacy/general/deathmaze/db/hooks/PlayerStatsSQLHook.java
+++ src/main/java/com/georlegacy/general/deathmaze/db/hooks/PlayerStatsSQLHook.java
@@ -5,6 +5,11 @@ import com.georlegacy.general.deathmaze.objects.PlayerStats;
 
 public class PlayerStatsSQLHook extends SQLHook.ISQLHook<PlayerStats> {
 
+    //todo fixed to compile
+    protected PlayerStatsSQLHook(SQLHook dbHook) {
+        super(dbHook);
+    }
+
     @Override
     public PlayerStats decode() {
         return null;
